f2f3ed481e6ccb600e374e50b45c83956c79169a
Abel-Git2103 <abel_jornet@hotmail.com>
2022-02-23 20:19:53 +0100

    INICIO

---
+++ src/main/java/mainAPP/Ta25Ud253Application.java
@@ -0,0 +1,13 @@
+package mainAPP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ta25Ud253Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ta25Ud253Application.class, args);
+	}
+
+}
